940d57e684c1a5029be8c1399855f3755f7b1d56
Damanpreet <daman.4880@gmail.com>
2016-05-20 21:54:42 +0530

    Added basic files

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var Box = React.createClass({
-	
+
 	render: function(){
-		
+
 		return(
 			<center>
 				<h1> Welcome! <br/> Site under Construction</h1>
 			</center>
 		)
 	}
-	
+
 });
 
 ReactDOM.render(
 	<Box/>,
-	document.getElementById("container")
+	document.getElementById("main_container")
 );
