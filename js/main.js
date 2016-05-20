a4f8f236644353b1e18f078e38ecd105eeed099b
Damanpreet <daman.4880@gmail.com>
2016-05-20 21:36:12 +0530

    Started from scratch using react.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var Box = React.createClass({
+	
+	render: function(){
+		
+		return(
+			<center>
+				<h1> Welcome! <br/> Site under Construction</h1>
+			</center>
+		)
+	}
+	
+});
+
+ReactDOM.render(
+	<Box/>,
+	document.getElementById("container")
+);
