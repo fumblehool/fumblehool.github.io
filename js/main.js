26a4e5ab72441d5454df5c8fce46e153bbe16a3d
Damanpreet <daman.4880@gmail.com>
2016-05-21 14:03:18 +0530

    Added navigation and Jquery

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ var Banner = React.createClass({
 	render: function(){
 
 		return(
-			<div idName="border">
+			<div className="border">
 			<center>
-				<h1> Title</h1>
+				<img className="banner" src="img/banner.jpg"/>
 			</center>
 			</div>
 		)
@@ -17,11 +17,34 @@ var Navbar = React.createClass({
 	render: function(){
 
 		return(
-			<div className="border">
-			<center>
-				<h1> This is Navbar</h1>
-			</center>
-			</div>
+			<div>
+			<header className="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
+        <div className="container">
+            <div className="navbar-header">
+               <button className="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a href="index.html" className="navbar-brand">fumblehool</a>
+            </div>
+            <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                <form className="navbar-form navbar-right" role="search">
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Search"/>
+                    </div>
+                    <button type="submit" className="btn btn-default"> Submit</button>
+                </form>
+                <ul className="nav navbar-nav">
+                    <li className="active"><a href="#">Home</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+		</div>
 		)
 	}
 });
@@ -64,8 +87,8 @@ var Main = React.createClass({
 
 		return(
 			<div>
-				< Banner />
 				< Navbar />
+				< Banner />
 				< Content />
 				< Footer />
 				</div>
