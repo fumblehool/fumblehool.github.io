f94a2cd218dfec976efcafe3fbfa43f6e1ba7891
Damanpreet Singh <daman.4880@gmail.com>
2016-06-05 13:15:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,35 @@
- var data = [
- 						{post:'The first test post'},
- 						{post:'The Second test post'},
- 						{post:'The Third test post'}
+var data = [
+          {post:'The first test post'},
+          {post:'The Second test post'},
+          {post:'The Third test post'}
  ];
 
 var nav_data = ['Home','Contact','About'];
 
- var Post = React.createClass({
-
- 	render: function(){
- 		return(
- 			<div className="">
- 			<center>
-			<p className="jumbotron less-top-padding">
-				{this.props.title}<br/>
-				{this.props.data}
-			</p>
-			<br/>
- 			</center>
- 			</div>
- 		)
- 	}
+var Post = React.createClass({
+    render: function(){
+        return(
+ 	    <div className="">
+ 	    <center>
+	    <p className="jumbotron less-top-padding">
+                {this.props.title}<br/>
+                {this.props.data}
+            </p><br/>
+            </center>
+            </div>
+ 	)}
  });
 
 var Banner = React.createClass({
-
-	render: function(){
-
-		return(
-			<div className="">
-			<center>
-				<img className="banner" src="img/banner.jpg"/>
-			</center>
-			</div>
-		)
-	}
+    render: function(){
+        return(
+	    <div className="">
+	    <center>
+	        <img className="banner" src="img/banner.jpg"/>
+	    </center>
+	    </div>
+	)}
 });
-
 
 var Content = React.createClass({
 	//
@@ -51,127 +44,102 @@ var Content = React.createClass({
 	// 	return data;
 	// }
 
-	render: function(){
+    render: function(){
+        var row = [];
+        this.props.data.forEach(function(dt){
+            row.push(<Post data={dt.post} title="Post Title"/>);
+	});
 
-		var row = [];
-		this.props.data.forEach(function(dt){
-				row.push(<Post data={dt.post} title="Post Title"/>);
-		});
-		return(
-
-			<div className="content">
-			<center>
-			<div className="page-header">
-				<h1> Posts</h1>
-			</div>
-
-			{row}
-			</center>
-			</div>
-
-		)
-	}
+        return(
+            <div className="content">
+	    <center>
+            <div className="page-header">
+                <h1> Posts</h1>
+            </div>
+            {row}
+            </center>
+            </div>
+	)}
 });
 
 var Footer = React.createClass({
-
-	render: function(){
-
-		return(
-
-			<div className="center content">
-			<footer>
-        <center>
-        <hr/>
-        <p class="text-center">
-            Copyright © Daman. All rights reserved.
-        </p>
-        </center>
-    </footer>
-		</div>
-
-		)
-	}
+    render: function(){
+        return(
+            <div className="center content">
+            <footer>
+            <center>
+            <hr/>
+            <p class="text-center">
+                Copyright © Daman. All rights reserved.
+            </p>
+            </center>
+            </footer>
+	    </div>
+	)}
 });
 
 var Navbar = React.createClass({
+    getInitialState: function(){
+        return{ focused:  0 };
+    },
 
-  getInitialState: function(){
-      return{ focused:  0 };
-  },
+    clicked: function(number){
+        this.setState({
+            focused: number
+        })
+    },
 
-  clicked: function(number){
-      this.setState({
-          focused: number
-      })
-  },
-
-	render: function(){
-    var self = this;
-
-		return(
-      <div>
-        <header className="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
-           <div className="container">
-               <div className="navbar-header">
-                  <button className="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-                       <span className="sr-only">Toggle navigation</span>
-                       <span className="icon-bar"></span>
-                       <span className="icon-bar"></span>
-                       <span className="icon-bar"></span>
-                  </button>
-                  <a href="index.html" className="navbar-brand">fumblehool</a>
-               </div>
-            <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-               <form className="navbar-form navbar-right" role="search">
-                  <div className="form-group">
-                     <input type="text" className="form-control" placeholder="Search"/>
-                  </div>
-                     <button type="submit" className="btn btn-default"> Submit</button>
-               </form>
-
-
-          <ul className="nav navbar-nav">
-
-          {this.props.items.map(function(item,m){
-            var style = '';
-
-             if ( self.state.focused == m){
-                 style = 'active'
-             }
-
-             return <li className={style} onClick={self.clicked.bind(self,m)}><a href="#">{item}</a></li>
-
-          })}
-          </ul>
-          </nav>
-          </div>
-
-          </header>
-
-
-      </div>
-		)
-	}
+    render: function(){
+        var self = this;
+        return(
+            <div>
+            <header className="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
+            <div className="container">
+                <div className="navbar-header">
+                    <button className="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <a href="index.html" className="navbar-brand">fumblehool</a>
+                </div>
+                <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                    <form className="navbar-form navbar-right" role="search">
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Search"/>
+                    </div>
+                        <button type="submit" className="btn btn-default"> Submit</button>
+                    </form>
+                <ul className="nav navbar-nav">
+                    {this.props.items.map(function(item,m){
+                    var style = '';
+                    if ( self.state.focused == m){
+                        style = 'active'
+                    }
+                return <li className={style} onClick={self.clicked.bind(self,m)}><a href="#">{item}</a></li>
+                })}
+                </ul>
+                </nav>
+                </div>
+            </header>
+            </div>
+	)}
 });
 
-
 var Main = React.createClass({
-
-	render: function(){
-
-		return(
-			<div>
-				< Navbar items={nav_data} />
-				< Banner />
-				< Content data={data} />
-				< Footer />
-				</div>
-		)
-	}
+    render: function(){
+        return(
+	    <div>
+	        < Navbar items={nav_data} />
+	        < Banner />
+	        < Content data={data} />
+	        < Footer />
+	    </div>
+    )}
 });
 
 ReactDOM.render(
-	<Main/>,
-	document.getElementById("main_container")
+    <Main/>,
+    document.getElementById("main_container")
 );
